fef2cecf9fff2b9c731a732cf5528c83d210163f
THANGA-RAJU <106445608+THANGA-RAJU@users.noreply.github.com>
2024-10-10 23:09:41 +0530

    First commit

---
+++ src/StringSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class StringSort {
+    public static void main(String[] args) {
+//        String input = "example";
+//        String sortedString = sortString(input);
+//        System.out.println("Sorted string: " + sortedString);
+//    }
+//
+//    public static String sortString(String str) {
+//        char[] charArray = str.toCharArray(); 
+//        Arrays.sort(charArray); 
+//        return new String(charArray); 
+//    }
+    	
+    	String s="SpringBoot";
+    	char[] charArray=s.toCharArray();
+    	Arrays.sort(charArray);
+    	System.out.println(charArray);
+    	
+    	
+    	
+    }
+}
